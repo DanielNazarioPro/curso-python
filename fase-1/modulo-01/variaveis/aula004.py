241b44e5fe887a5bf084f4d70d7f7722a7ee2e71
DanielNazarioPro <dmalzinha0221@gmail.com>
2026-07-22 18:50:53 -0400

    texto no python

---
+++ fase-1/modulo-01/variaveis/aula004.py
@@ -0,0 +1,24 @@
+# texto em python
+
+print("Python")
+print('Meu nome é Daniel')
+
+
+print(type("Python")) # str
+print(type("500")) # str
+print(type(500)) # int
+
+print(int('5')) # type: ignore # converte string para inteiro'
+print(float('5.5')) # type: ignore # converte string para float'
+print(str(5)) # type: ignore # converte inteiro para string'
+print(str(5.5)) # type: ignore # converte float para string'
+
+print(bool(1)) # type: ignore # converte inteiro para booleano'
+
+print("50" + "10") # type: ignore # concatenação de strings
+
+print(int("50") + int("10")) # type: ignore # soma de números representados por strings
+
+print ("olá " + "   Daniel") # type: ignore # concatenação de strings
+
+print ("python " * 3) # type: ignore # repete a string 3 vezes
