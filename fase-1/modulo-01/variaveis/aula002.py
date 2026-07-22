0a47c65b69d402614a4dcf5920c348af5c52db88
DanielNazarioPro <dmalzinha0221@gmail.com>
2026-07-22 18:42:19 -0400

    Números em Python

---
+++ fase-1/modulo-01/variaveis/aula002.py
@@ -0,0 +1,45 @@
+# Números em Python
+
+"""
+Função print() é utilizada para exibir informações na tela.
+dentro do parênteses, podemos colocar números, textos ou variáveis.
+que será exibido no terminal.
+
+Aqui está alguns exemplos de operações matemáticas em Python:
+"""
+print (1 + 2) # Adição
+
+print (5 - 3) # Subtração
+
+print (4 * 2) # Multiplicação
+
+print (10 / 2) # Divisão
+
+print (5 ** 2) # Exponenciação
+
+print (10 % 3) # Resto da divisão
+
+print (10 // 3) # Divisão inteira
+
+print (10 > 5) # Maior que
+
+print (10 < 5) # Menor que
+
+print (10 == 5) # type: ignore # Igual a
+
+print (10 != 5) # type: ignore # Diferente de
+
+print (10 >= 5) # Maior ou igual a
+
+print (10 <= 5) # Menor ou igual a
+
+"""
+O python pode fazer expressão matemáticas complexas, como por exemplo:
+"""
+print (10 + 5 * 2) # A multiplicação tem prioridade sobre a adição
+
+print((10 + 5) * 2) # O uso de parênteses altera a prioridade das operações
+
+print (10 / 3) # O resultado é um número decimal (float)
+
+
