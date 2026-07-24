e2f803991959ccdc2b365c1134af817cb8e8e6d5
DanielNazarioPro <dmalzinha0221@gmail.com>
2026-07-23 22:45:26 -0400

    Como usar String Metodos e Concatenações no Python

---
+++ fase-1/modulo-01/aulas/007-String,MetodoeConcatenacao.py
@@ -0,0 +1,39 @@
+# Strings
+"""
+1-  método title()
+2-  método upper() / lower()
+3- Concatenação de strings. (+)
+4- White space em strings: \n(-> faz uma quebra de linha)  e \t (-> faz uma tabulação)
+5- Removendo espaços em Python: rstrip(), lstrip() e strip()
+6- Quando usar as aspas simples e aspas duplas.
+"""
+"""
+nome = 'daniel nazario
+print(nome.title())
+print(nome.upper())
+print(nome.lower())
+
+primeiroNome = 'daniel'
+segundoNome = 'nazario'
+nomeCompleto = primeiroNome +' '+ segundoNome
+print('Olá ' +nomeCompleto+ '!')
+
+print('nome: \nfelipe')
+print('Linguagem de programação: \tPython\tJava\tJavascript')
+nome = ' daniel '
+obs = '1'
+# metodo rstrip() (-> apaga os campos em branco do lado direito)
+print(nome.strip() + obs)
+print(obs + nome.strip())
+# metodo lstrip() (-> apaga os campos em branco do lado esquerdo)
+print(nome.lstrip() + obs)
+print(obs + nome.lstrip() + obs)
+# metodo stripe() (-> apaga todos os campos em branco.)
+print(nome.strip() + obs)
+print(obs + nome.strip() + obs)"""
+
+
+frase = "Eu vou para John's bar"
+frase1 = 'Maria disse: "passei na prova!"'
+print(frase)
+print(frase1)
